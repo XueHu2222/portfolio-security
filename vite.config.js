799863588e510e1135cb1e10149d3d43aee7022e
Xue Hu <hu0018@hz.nl>
2025-06-13 22:58:53 +0200

    Fix vite base and HTTPS asset loading

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-    base: 'https://posts-app-95ae69b11cf9.herokuapp.com/',
+    base: '/build/',
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
